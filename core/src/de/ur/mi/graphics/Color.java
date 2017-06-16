12db638e515f286ddea60ae70a8ba7888359d5ed
FS <florin.schwappach@ur.de>
2017-06-16 15:48:07 +0200

    Added the GraphicsApp methods setup and draw, as well as the graphics object management. Resizing and general initial app setup is still a stub.

---
+++ core/src/de/ur/mi/graphics/Color.java
@@ -0,0 +1,7 @@
+package de.ur.mi.graphics;
+
+/**
+ * Created by LocalAdmin on 16.06.2017.
+ */
+public class Color {
+}
--- core/src/de/ur/mi/graphicsapp/GraphicsApp.java
+++ core/src/de/ur/mi/graphicsapp/GraphicsApp.java
@@ -5,15 +5,36 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import de.ur.mi.graphics.GraphicsObject;
+
+import java.io.File;
+import java.net.URL;
+import java.util.WeakHashMap;
 
 public class GraphicsApp extends ApplicationAdapter {
+
 	private SpriteBatch batch;
 	private Texture img;
-	
+
+    private WeakHashMap<GraphicsObject, GraphicsObject> graphicsObjects;
+
+    public int canvasWidth;
+    public int canvasHeight;
+
+	///////////////////////////////////////////////////////////////////////////
+
+	// Lifecycle methods inherited from ApplicationAdapter
+
+	///////////////////////////////////////////////////////////////////////////
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+        graphicsObjects = new WeakHashMap<GraphicsObject, GraphicsObject>();
+
+        batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+        setup();
 	}
 
 	@Override
@@ -23,11 +44,79 @@ public class GraphicsApp extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+
+        draw();
 	}
+
+	@Override
+    public void resize(int width, int height) {
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+    }
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
 	}
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Interface for GraphicsObject management
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    public void addObject(GraphicsObject object) {
+        graphicsObjects.put(object, object);
+    }
+
+    public void removeObject(GraphicsObject object) {
+        graphicsObjects.remove(object);
+    }
+
+    public void removeAllObjects() {
+        graphicsObjects.clear();
+    }
+
+	///////////////////////////////////////////////////////////////////////////
+
+	// Interface for Exercises
+
+	///////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Contains the code to be executed, ONCE, on start of the program. If you
+	 * are defining your own GraphicsApp class, you need to override the
+	 * definition of setup so that it contains the code for your application.
+	 */
+	public void setup() {
+
+	}
+
+    /**
+     * Contains the code to be executed, REPEATEDLY, during execution of the
+     * program. Override this method to animate graphical graphicsObjects.
+     */
+	public void draw() {
+
+	}
+
+    /**
+     * Loads a file from source directory.
+     *
+     * @param path Path to file, relative to 'src' directory
+     * @return A File object
+     */
+    public File loadFile(String path) {
+        URL base = GraphicsApp.class.getResource(path);
+        return new File(base.getFile());
+    }
+
+    public int getWidth(){
+        return canvasWidth;
+    }
+
+    public int getHeight(){
+        return canvasHeight;
+    }
 }
